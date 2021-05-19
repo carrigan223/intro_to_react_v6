e1edd48dddedc9973c95c2f468b4c698dcadc0af
Andrew Carrigan <carrigan223@gmail.com>
2021-05-19 14:06:12 -0700

    playing with tailwinds

--- src/App.js
+++ src/App.js
@@ -9,10 +9,27 @@ const App = () => {
   const theme = useState("darkgreen");
   return (
     <ThemeContext.Provider value={theme}>
-      <div>
+      <div
+        className="p-0 m-0"
+        style={{
+          background:
+            "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+        }}
+      >
         <Router>
-          <header>
-            <Link to="/">
+          <header
+            className="
+                      w-full 
+                      mb-10 
+                      text-center 
+                      p-7 
+                      bg-gradient-to-b 
+                      from-purple-400 
+                      via-pink-400 
+                      bg-red-500
+                      "
+          >
+            <Link to="/" className="text-white  text-6xl hover:text-blue-700">
               <h1>Adopt Me!</h1>
             </Link>
           </header>
